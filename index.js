11849e71ede2d4551fee5c55b9f611a729e60ec5
ankitjain28may <ankitjain28may77@gmail.com>
2019-03-24 23:30:34 +0530

    integrate CD pipeline

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 9000;
 
 app.get('/', (req, res) => {
-  res.send("Hello world, Looking to automate this stuff on heroku..!");
+  res.send("Hello world, Yes it is working..!");
 })
 
 app.listen(port);
